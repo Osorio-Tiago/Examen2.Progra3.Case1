3d11b330bf1bf5c6f978eb2b93de3286e8045e8d
Ariadna_Ch <ariadna.c2405@gmail.com>
2021-06-24 23:07:02 -0600

    Cambios WeatherStats

--- Examen2.Progra3.Case1/src/WeatherStats.java
+++ Examen2.Progra3.Case1/src/WeatherStats.java
@@ -12,9 +12,9 @@ public class WeatherStats extends WeatherDataDecorator{
 	
 
     
-    private final List<Double> temperatureRecord = new ArrayList();
-    private final List<Double> humidityRecord = new ArrayList();
-    private final List<Double> barometricRecord = new ArrayList();
+    private final List<Double> temperatureInformation = new ArrayList();
+    private final List<Double> humidityInformation = new ArrayList();
+    private final List<Double> barometricInformation = new ArrayList();
     
 
     
@@ -29,83 +29,87 @@ public class WeatherStats extends WeatherDataDecorator{
         System.out.println("|Estadísticas Meteorológicas             |");
         System.out.println("+=====+==================================+");    
         System.out.println("|[1]- Información Temperatura:           ");
-        averageValue(temperatureRecord);
-        maximumValue(temperatureRecord);
-        minimumValue(temperatureRecord);
+        averageValue(temperatureInformation);
+        maximumValue(temperatureInformation);
+        minimumValue(temperatureInformation);
         System.out.println("|________________________________________");
         
         System.out.println("|[2]- Humedad                            ");
-        averageValue(humidityRecord);
-        maximumValue(humidityRecord);
-        minimumValue(humidityRecord);
+        averageValue(humidityInformation);
+        maximumValue(humidityInformation);
+        minimumValue(humidityInformation);
         System.out.println("|________________________________________");
         
         System.out.println("|[3]- Presion Barometrica                ");
-        averageValue(barometricRecord);
-        maximumValue(barometricRecord);
-        minimumValue(barometricRecord);
+        averageValue(barometricInformation);
+        maximumValue(barometricInformation);
+        minimumValue(barometricInformation);
         System.out.println("|________________________________________ \n");
     }
 
     @Override
     public void update(double temperature, double humidity, double barometricPressure) {
         super.update(temperature, humidity, barometricPressure);
-        temperatureRecord.add(temperature);
-        humidityRecord.add(humidity);
-        barometricRecord.add(barometricPressure);
+        temperatureInformation.add(temperature);
+        humidityInformation.add(humidity);
+        barometricInformation.add(barometricPressure);
     }
+    
+    /*
+     * El método average nos permite recorrer la lista de reportes compuesta por la 
+     * data observada y así sacar un promedio general de los valores que 
+     * la componen en cada elemento característico.
+     * 
+     */     
+        public void averageValue(List<Double> value){
+            double promedio = 0;
+            
+            for(int i = 0; i < value.size(); i++){
+                promedio += value.get(i);
+            }
+            
+            System.out.println("|Promedio: " + promedio/value.size());
+        }
   
+	/*
+	 * El método mínimo nos permite recorrer la lista de reportes compuesta por la
+	 * data observada y así sacar los valores más bajos que la componen en cada
+	 * característica.
+	 * 
+	 */
+	public void minimumValue(List<Double> value) {
+		double minimum = 0;
+
+		for (int i = 0; i < value.size(); i++) {
+			if (minimum == 0)
+				minimum = value.get(i);
+
+			if (minimum > value.get(i))
+				minimum = value.get(i);
+		}
+
+		System.out.println("|Minimo: " + minimum);
+	}
 /*
  * El método máximo nos permite recorrer la lista de reportes compuesta por la 
  * data observada y así sacar los valores más altos que la componen en cada
  * característica.
  * 
  */    
-    public void maximumValue(List<Double> record){
-        double max = 0;
+    public void maximumValue(List<Double> value){
+        double maximum = 0;
         
-        for(int i = 0; i < record.size(); i++){
-            if(max==0)
-                max = record.get(i);
+        for(int i = 0; i < value.size(); i++){
+            if(maximum==0)
+                maximum = value.get(i);
                 
-            if(max<record.get(i))
-                max = record.get(i);            
+            if(maximum<value.get(i))
+                maximum = value.get(i);            
         }
         
-        System.out.println("|Maximo: " + max);
+        System.out.println("|Maximo: " + maximum);
     }
-/*
- * El método mínimo nos permite recorrer la lista de reportes compuesta por la 
- * data observada y así sacar los valores más bajos que la componen en cada
- * característica.
- * 
- */     
-    public void minimumValue(List<Double> record){
-        double min = 0;
 
-        for(int i = 0; i < record.size(); i++){
-            if(min==0)
-                min = record.get(i);
-                
-            if(min>record.get(i))
-                min = record.get(i);            
-        }
-        
-        System.out.println("|Minimo: " + min);
-    }
-/*
- * El método average nos permite recorrer la lista de reportes compuesta por la 
- * data observada y así sacar un promedio general de los valores que 
- * la componen en cada elemento característico.
- * 
- */     
-    public void averageValue(List<Double> record){
-        double promedio = 0;
-        
-        for(int i = 0; i < record.size(); i++){
-            promedio += record.get(i);
-        }
-        
-        System.out.println("|Promedio: " + promedio/record.size());
-    }
+
+    
 }
