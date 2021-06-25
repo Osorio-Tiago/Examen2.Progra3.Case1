7b6228415ac1348fff0c1a13c149c04b138a1d4c
moise <moise@DESKTOP-B1V183G>
2021-06-24 21:07:13 -0600

    comentarios main

--- Examen2.Progra3.Case1/src/Main.java
+++ Examen2.Progra3.Case1/src/Main.java
@@ -1,6 +1,25 @@
+/*
+ * Examen II - Justificación de los patrones seleccionados - Caso I:
+ * 
+ * Para el primer set partiendo de las necesidades mencionadas en el enunciado respecto a crear 3
+ * elementos de visualización los cuales dependen de una actualización constante en dipositivo principal
+ * tomamos la decisión de utilizar el patron "Observer" ya que nos da gran facilidad a la hora de querer 
+ * actualizas los elementos de visualización de una manera eficiente por medio de un Subject y Observers.  
+ * 
+ * Para el segundo set respecto a las necesidades de crear dispositivos hibridos los cuales combinan 
+ * 2 o más elementos de visualización tomamos la decisión de utilizar el patron Decorador el cual nos ayuda 
+ * tanto  en el primer set a crear un solo "dispositivoGeneral" demostrando los 3 elemento de 
+ * visualización sino que tambien nos ayuda a crear dipositivos hibridos con diferentes combinaciones, 
+ * eso ayudará a Weather-ORama a tomar solo las combinaciones deseadas para sus futuras API. 
+ * 
+ * 
+ * */
+
+
+
 
 public class Main {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
